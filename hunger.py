08c718ac53e9dc1e396e054575217e2b9ae9f033
Arghya jain <arghyadprasad123@gmail.com>
2024-02-03 21:42:05 +0530

    some changes

--- hunger.py
+++ hunger.py
@@ -1,1 +1,8 @@
-print("I'm you hungry")
+h = input('Are you hungry?\n')
+if h == 'yes':
+    print('eat pizza')
+elif h == 'no':
+    t = input('Are you thirsty?\n')
+    if t == 'yes':
+        print('drink water')
+
